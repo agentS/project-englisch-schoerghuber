a3275c9759e1aa649a8d7a8f4cbccb0819a33d6c
Lukas Schoerghuber <lukas.schoerghuber@posteo.at>
2020-06-08 13:12:14 +0200

    Integrate SpringDoc OpenAPI
    This commit integrates a (faulty) version of SpringDoc OpenAPI which
    fetches the OpenAPI documents from the proxied services and unifies
    them via OpenAPI groups.

---
+++ gateway/src/main/java/eu/nighttrains/gateway/configuration/GatewayConfiguration.java
@@ -0,0 +1,11 @@
+package eu.nighttrains.gateway.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+}
