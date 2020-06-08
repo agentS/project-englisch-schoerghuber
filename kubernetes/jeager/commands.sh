a2b0dedc9d6cb66891be555aac7744f229a920e9
Lukas Schoerghuber <lukas.schoerghuber@posteo.at>
2020-06-08 14:48:10 +0200

    Add file for building booking serivce docker container

--- kubernetes/jeager/commands.sh
+++ kubernetes/jeager/commands.sh
@@ -3,3 +3,5 @@
 kubectl create -f jaeger-configuration.yaml
 kubectl create -f jaeger-deployment.yaml
 kubectl create -f jaeger-service.yaml
+kubectl create -f jaeger-ui-service.yaml
+
--- kubernetes/jeager/delete.sh
+++ kubernetes/jeager/delete.sh
@@ -3,3 +3,4 @@
 kubectl delete -f jaeger-configuration.yaml
 kubectl delete -f jaeger-deployment.yaml
 kubectl delete -f jaeger-service.yaml
+kubectl delete -f jaeger-ui-service.yaml
