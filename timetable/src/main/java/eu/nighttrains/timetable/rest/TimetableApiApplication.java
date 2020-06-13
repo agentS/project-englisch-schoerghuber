5d3c7857843923213973effa4b7055fa02f6f0e9
Lukas Schoerghuber <lukas.schoerghuber@posteo.at>
2020-06-13 09:59:13 +0200

    Authenticate requests to booking service using OAuth
    This commit adds the ability to authenticate requests to the booking
    service using OAuth with Okta as backend. While all requests previously
    were unauthenticated, this commit enables authentication for requests
    to the booking service and token forwarding from the gateway to the
    booking service.

--- timetable/src/main/java/eu/nighttrains/timetable/rest/TimetableApiApplication.java
+++ timetable/src/main/java/eu/nighttrains/timetable/rest/TimetableApiApplication.java
@@ -23,7 +23,7 @@ import javax.ws.rs.core.Application;
                         url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
                 )
         ),
-        servers = @Server(url = "http://192.168.99.100:30272/timetable/", description = "Timetable API development server"),
+        servers = @Server(url = "http://localhost:8081/timetable/", description = "Timetable API development server"),
         tags = @Tag(name = TimetableApiApplication.OPEN_API_TAG_NAME_TIMETABLE, description = "Timetable-related endpoints")
 )
 @ApplicationPath("/")
